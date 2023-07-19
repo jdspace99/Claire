0c9dd806e07d4d46d20aa47a127cf11040d063f6
EC2 Default User <ec2-user@ip-172-31-84-224.ec2.internal>
2023-07-19 01:01:14 +0000

    Second commit completed

---
+++ hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "My name is Olajide"
+echo "I work for Amazon"
+mkdir Kudenda
+echo "Today is : "
+date
---
+++ username.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo Enter your name
+read name
+echo Enter your company
+read company
+echo Enter the country you work
+read country
+
+echo $name works for $company in $country
+
